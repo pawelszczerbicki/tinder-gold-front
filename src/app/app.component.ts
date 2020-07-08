09b826442844d5c7bd26b90e2264b88381310960
Paweł Szczerbicki <szczerbicki.pawel@gmail.com>
2020-07-08 22:20:47 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'front';
+  @ViewChild('token') token: ElementRef;
+  loading;
+  error;
+  users;
+
+  constructor(private http: HttpClient) {
+  }
+
+  fetchUsers() {
+    this.loading = true;
+    const token = this.token.nativeElement.value;
+    this.http.post(`${environment.apiUrl}/tinder`, {token}).toPromise()
+      .then(res => this.users = res)
+      .catch(() => this.error = true)
+      .finally(() => this.loading = false);
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  apiUrl: 'https://arcane-mesa-69842.herokuapp.com',
   production: true
 };
